81657545ec8f7e430fd9645f60fb4bb80458c333
Sachin <lavudyasachinchavan@gmail.com>
2024-01-07 22:48:24 +0530

    added properties

---
+++ src/components/aboutus/Properties/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+const PropertiesHero = () => {
+  return (
+    <div className={styles.main_cont}>
+      PropertiesHero is yet to develop
+      <h2 className={styles.heading}>Properties for Sale</h2>
+      <p className={styles.tagline}>Find properties in your favourite city</p>
+      <div className={styles.prop_cont}>
+        
+        <div className={styles.prop_list}></div>
+        <div className={styles.latest}>hi</div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertiesHero;
---
+++ src/pages/properties.tsx
@@ -0,0 +1,12 @@
+import PropertiesHero from '@/components/aboutus/Properties';
+import React from 'react';
+
+const Properties = () => {
+  return (
+    <div>
+      <PropertiesHero />
+    </div>
+  );
+};
+
+export default Properties;
