f4c01d0d633477ee103242862689e93c2674a861
L Sachin <lsachin@linkfields.com>
2024-03-06 17:29:04 +0530

    added flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./node_modules/flowbite-react/lib/**/*.js",
+      "./pages/**/*.{ts,tsx}",
+      "./public/**/*.html",
+    ],
+    plugins: [
+      require("flowbite/plugin")
+    ],
+    theme: {},
+  };
