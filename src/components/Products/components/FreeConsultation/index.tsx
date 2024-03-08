69e56cf059693f5af49ade3ce3d65ce0e29e390a
L Sachin <lsachin@linkfields.com>
2024-03-08 19:10:47 +0530

    added contact us in builder

--- src/components/Products/components/FreeConsultation/index.tsx
+++ src/components/Products/components/FreeConsultation/index.tsx
@@ -13,9 +13,13 @@ const FreeConsultation = () => {
 
   return (
     <div className={styles.main_container}>
-      <div>
+      <div className={styles.img_back_cont}>
+        <p className={styles.home_title}>Construct your dream 
+        <br></br><span className={styles.hightlight}>house as you like</span> </p>
+      </div>
+      <div className={styles.contact_us_cont}>
         <form onSubmit={handleSubmit}>
-          <h1 className={styles.title_cont}>GET A FREE CONSULTATION</h1>
+          <h1 className={styles.title_cont}>Expert quotation, inquire now.</h1>
           <div className={styles.input_cont}>
             <label>Name</label>
             <div className={styles.input_cont}>
@@ -51,14 +55,27 @@ const FreeConsultation = () => {
             </div>
           </div>
           <div className={styles.input_cont}>
-            <label>Location</label>
-            <div className={styles.location_cont}>
-              <select>
-                <option>Hyderabad</option>
-                <option>Secundrabad</option>
-                <option>Pune</option>
-                <option>Mumbai</option>
-              </select>
+            <div className={styles.email_loc_cont}>
+              <div className={styles.location}>
+                <label>Location</label>
+                <div className={styles.location_cont}>
+                  <select>
+                    <option>Hyderabad</option>
+                    <option>Secundrabad</option>
+                    <option>Pune</option>
+                    <option>Mumbai</option>
+                  </select>
+                </div>
+              </div>
+              <div className={styles.email_cont}>
+                <label>Email</label>
+                <input
+                  type="tel"
+                  value={phoneNo}
+                  onChange={(e) => setPhoneNo(e.target.value)}
+                  placeholder="Enter the email"
+                />
+              </div>
             </div>
           </div>
           <div className={styles.btn_container}>
